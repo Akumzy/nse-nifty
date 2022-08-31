3b775ee6140344a8a2c758665f5c38b8697d171a
Promise <pebuka351@gmail.com>
2022-08-31 02:39:52 -0700

    commited now

---
+++ ersakumapromisense-niftypagesindex.tsx
@@ -0,0 +1,77 @@
+[33mcommit 8093370d547bc7a5a1ec8584d4b7edbfe86f7afd[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Promise <pebuka351@gmail.com>
+Date:   Tue Aug 30 02:37:59 2022 -0700
+
+    commited
+
+[33mcommit a5398f0747bb96075e3e5bd9db4e829c76563948[m
+Author: Promise <pebuka351@gmail.com>
+Date:   Tue Aug 30 02:34:42 2022 -0700
+
+    Committed
+
+[33mcommit edc74ab6144fa4adc9804d3d2c626df14bf1763b[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Wed Jul 20 19:27:56 2022 +0100
+
+    resolved issues
+
+[33mcommit f50b6806ed6bf7dd68f183fab946d0de7f726016[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 17:21:07 2022 +0100
+
+    updates
+
+[33mcommit dde3976c091dfe28b814a49337bbce9b8e74a34e[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 14:28:18 2022 +0100
+
+    fix
+
+[33mcommit d00c39173c133a803ca9b3cf2fb033f4f80b6adc[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 14:13:12 2022 +0100
+
+    f
+
+[33mcommit 00432208bfdab8002d67d1de41398441b4845184[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 14:03:17 2022 +0100
+
+    test
+
+[33mcommit 183986652e147e3d2b9d260eb7484f1317fe3879[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 13:06:36 2022 +0100
+
+    fetch using axios
+
+[33mcommit f26a9c338793d8f8dfa58bb4b29f16acd72dea88[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 12:09:58 2022 +0100
+
+    added catch
+
+[33mcommit 8d7dcca64ec5755679dd607e6f84bd7633f2fb45[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 12:06:21 2022 +0100
+
+    update
+
+[33mcommit 1689add6d2d3deaf6a454b3b1a2c4899b2b2bcc7[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 11:42:14 2022 +0100
+
+    Updated readme
+
+[33mcommit 5b13c5f238bdd89cc6283dc487b1265bdc21146e[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 11:38:48 2022 +0100
+
+    init
+
+[33mcommit dc6946c1e168f5774053a129f52d2364ce09d78b[m
+Author: Akuma Isaac Akuma <akumaisaacakuma.com>
+Date:   Tue Jul 19 11:38:39 2022 +0100
+
+    first commit
